5e94f69b5665d08693b14ad1d548af644304c3b3
Boris Rasin <boris@pointx.org>
2020-07-30 00:09:52 +0300

    update scapix

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  scapix 1.0.17
-  URL "https://github.com/scapix-com/scapix/archive/v1.0.17.tar.gz"
-  URL_HASH SHA256=260b87e07065b1e79c973ddb9a60721a57503cff416b92e3fd4d4791d9a5240b
+  scapix 1.0.18
+  URL "https://github.com/scapix-com/scapix/archive/v1.0.18.tar.gz"
+  URL_HASH SHA256=d3036cf8648467c2f2df8ba0c61ccf8ac9ced67b075aedf3489cbd2a9eefccfc
 )
