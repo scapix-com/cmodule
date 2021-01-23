1ba1862b7c0d3151d1a38e2a24b7ea6701094dc9
Boris Rasin <boris@pointx.org>
2021-01-23 03:12:11 +0200

    scapix-bin: add Darwin-arm64 platform

--- cmake/FindScapixBin.cmake
+++ cmake/FindScapixBin.cmake
@@ -1,6 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 set(scapix_bin_hash_Darwin-x86_64 ccce99886ee51e0fd2f505188cc1bae956f8a0b41fb4248d09c401e4720f6707)
+set(scapix_bin_hash_Darwin-arm64  825c432f31afc2d670d041bc3c17112e94a9d18ac63521cd8216ed8b4103b101)
 set(scapix_bin_hash_Linux-x86_64  3567c5f267ce8100dd420f339e054e16dfde70cddc3621031c7f351c1d66dde2)
 set(scapix_bin_hash_Windows-AMD64 a015ac3a03d52514a616be0e801610c388c9cd0fd965725e6d49ab19486800ad)
 
