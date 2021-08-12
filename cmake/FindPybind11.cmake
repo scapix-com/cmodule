0c5073def828b442a76d18ca7f10ac43545b0e2a
Boris Rasin <boris@pointx.org>
2021-08-12 16:15:23 +0300

    update pybind11 version 2.7.1

--- cmake/FindPybind11.cmake
+++ cmake/FindPybind11.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  pybind11 2.4.3-p0
-  URL "https://github.com/pybind/pybind11/archive/v2.4.3.tar.gz"
-  URL_HASH SHA256=1eed57bc6863190e35637290f97a20c81cfe4d9090ac0a24f3bbf08f265eb71d
+  pybind11 2.7.1
+  URL "https://github.com/pybind/pybind11/archive/v2.7.1.tar.gz"
+  URL_HASH SHA256=616d1c42e4cf14fa27b2a4ff759d7d7b33006fdc5ad8fd603bb2c22622f27020
 )
