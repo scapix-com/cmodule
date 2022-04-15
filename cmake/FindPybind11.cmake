ee70f0ef30e29ae60a7eaefab3cf425ce87e06ca
Boris Rasin <boris@pointx.org>
2022-04-16 00:36:32 +0300

    update pybind11 to version 2.9.2

--- cmake/FindPybind11.cmake
+++ cmake/FindPybind11.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  pybind11 2.9.1
-  URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.9.1.tar.gz"
-  URL_HASH SHA256=c6160321dc98e6e1184cc791fbeadd2907bb4a0ce0e447f2ea4ff8ab56550913
+  pybind11 2.9.2
+  URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.9.2.tar.gz"
+  URL_HASH SHA256=6bd528c4dbe2276635dc787b6b1f2e5316cf6b49ee3e150264e455a0d68d19c1
 )
