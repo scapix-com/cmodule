5a413a36f1dd277b689d4096138e41fbcd030719
Boris Rasin <boris@pointx.org>
2022-05-08 03:16:27 +0300

    scapix 1.0.29

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  scapix 1.0.28
-  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.28.tar.gz"
-  URL_HASH SHA256=34a5babef67d1597ebb4c8853d835e5d14691a39ce164efd7011b5916a825f9e
+  scapix 1.0.29
+  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.29.tar.gz"
+  URL_HASH SHA256=d5b82ab1d000b1d32d089f6fd3c9fdcd73ded916a37844f89701e961ba2f0652
 )
