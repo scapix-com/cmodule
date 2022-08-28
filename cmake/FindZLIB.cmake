6289bb5a17f01a4b08d3ad0ea86a52521956919b
Boris Rasin <boris@pointx.org>
2022-08-29 00:55:28 +0300

    update ZLIB to version 1.2.12

--- cmake/FindZLIB.cmake
+++ cmake/FindZLIB.cmake
@@ -1,9 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  zlib 1.2.11
-  URL      "https://zlib.net/zlib-1.2.11.tar.xz"
-  URL_HASH SHA256=4ff941449631ace0d4d203e3483be9dbc9da454084111f97ea0a2114e19bf066
+  zlib 1.2.12
+  URL      "https://zlib.net/zlib-1.2.12.tar.xz"
+  URL_HASH SHA256=7db46b8d7726232a621befaab4a1c870f00a90805511c0e0090441dac57def18
 )
 
 if(NOT TARGET ZLIB::ZLIB)
