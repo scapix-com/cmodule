a93b1c47be28631ce5f8ac51600919376d6045b5
Boris Rasin <boris@pointx.org>
2023-04-02 13:48:02 +0300

    scapix 1.0.32 / scapix-bin 2.0.14

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
 cmodule_add(
-  scapix 1.0.31
-  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.31.tar.gz"
-  URL_HASH SHA256=3942a8f50c0598120a047f4beaf3791d8c5097f2423ce982fd9f15ba5f5489c4
+  scapix 1.0.32
+  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.32.tar.gz"
+  URL_HASH SHA256=260d13c814139403de496b507825385241af2325a12a8728d9eecaae2537be87
 )
--- cmake/FindScapixBin.cmake
+++ cmake/FindScapixBin.cmake
@@ -1,13 +1,13 @@
 include(${CMAKE_CURRENT_LIST_DIR}/cmodule.cmake)
 
-set(scapix_bin_hash_Darwin-arm64  fdedbf0cd595fb902164086827fce6ac5be7c8b0a87f7ed8a5cc9cfc8330c3e1)
-set(scapix_bin_hash_Darwin-x86_64 5e8cb570168b0a92b928e53e4bdb78ded9b4002a2e77962d87ec0ffd8337eed1)
-set(scapix_bin_hash_Linux-x86_64  7a244f34d4d2540d54ac8996a911a775a2c6113037e5513ee458b8f8ab1f1eef)
-set(scapix_bin_hash_Windows-AMD64 351bdc08cf11e6d49c6cac5fe1033f582aa0525f5fd64f16d06cbf147c7e40fb)
+set(scapix_bin_hash_Darwin-arm64  db508da98c15d80addbd695c244245bbc6989f9e79982c55ae4141bb09188a2f)
+set(scapix_bin_hash_Darwin-x86_64 bb02ff42d720114c783045f4c2516c6ee2956fbf229ba1e204ce6f6a2a11f643)
+set(scapix_bin_hash_Linux-x86_64  fc6ef1e18e736d83501b4149ab20fe33960cf131515daa388ff492accacd01ce)
+set(scapix_bin_hash_Windows-AMD64 72a2471b43398113170c2ec6ad3696df176dd0da0f8f6c3095eb2447cb37c109)
 
 cmodule_add(
-  scapix_bin 2.0.13
-  URL "https://github.com/scapix-com/scapix-bin/archive/refs/tags/v2.0.13-${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}.tar.gz"
+  scapix_bin 2.0.14
+  URL "https://github.com/scapix-com/scapix-bin/archive/refs/tags/v2.0.14-${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}.tar.gz"
   URL_HASH SHA256=${scapix_bin_hash_${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_PROCESSOR}}
 )
 
