72a9216b5088127b3d7043b8193e497b32a1ed47
Boris Rasin <boris@pointx.org>
2023-08-19 01:58:48 +0300

    update curl to version 8.2.1

--- cmake/FindCURL.cmake
+++ cmake/FindCURL.cmake
@@ -5,7 +5,7 @@ include(cmodule)
 set(CURL_ENABLE_EXPORT_TARGET OFF)
 
 cmodule_add(
-  curl 7.80.0
-  URL "https://github.com/curl/curl/releases/download/curl-7_80_0/curl-7.80.0.tar.xz"
-  URL_HASH SHA256=a132bd93188b938771135ac7c1f3ac1d3ce507c1fcbef8c471397639214ae2ab
+  curl 8.2.1
+  URL "https://github.com/curl/curl/releases/download/curl-8_2_1/curl-8.2.1.tar.xz"
+  URL_HASH SHA256=dd322f6bd0a20e6cebdfd388f69e98c3d183bed792cf4713c8a7ef498cba4894
 )
