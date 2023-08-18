a8b6623aca72fef464b7dc7711d57e37c246c38a
Boris Rasin <boris@pointx.org>
2023-08-18 16:07:24 +0300

    update zlib to version 1.3

--- cmake/FindZLIB.cmake
+++ cmake/FindZLIB.cmake
@@ -3,9 +3,9 @@ include_guard(GLOBAL)
 include(cmodule)
 
 cmodule_add(
-  zlib 1.2.13
-  URL      "https://github.com/madler/zlib/releases/download/v1.2.13/zlib-1.2.13.tar.xz"
-  URL_HASH SHA256=d14c38e313afc35a9a8760dadf26042f51ea0f5d154b0630a31da0540107fb98
+  zlib 1.3
+  URL      "https://github.com/madler/zlib/releases/download/v1.3/zlib-1.3.tar.xz"
+  URL_HASH SHA256=8a9ba2898e1d0d774eca6ba5b4627a11e5588ba85c8851336eb38de4683050a7
 )
 
 cmodule_select_target(zlib zlibstatic)
