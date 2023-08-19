54bc4e0693fd9299fb01b15d38125b2007ac2e57
Boris Rasin <boris@pointx.org>
2023-08-19 12:50:02 +0300

    update scapix to version 1.0.36

--- cmake/FindScapix.cmake
+++ cmake/FindScapix.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 cmodule_add(
-  scapix 1.0.35
-  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.35.tar.gz"
-  URL_HASH SHA256=b8e9232326a6275a391a9b5539b312ea3ed7103e333d95b0fc3b1e599615cb48
+  scapix 1.0.36
+  URL "https://github.com/scapix-com/scapix/archive/refs/tags/v1.0.36.tar.gz"
+  URL_HASH SHA256=fc66a2fc5a0d039f080100b855fe8dd332136a3c8c2e98edffd4ffd994733621
 )
