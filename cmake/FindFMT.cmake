6cc71b078627b935475c40902c14208f6b6fa4ec
Boris Rasin <boris@pointx.org>
2024-01-18 16:53:09 +0200

    update fmt to version 10.2.1

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 option(FMT_INSTALL "" OFF)
 
 cmodule_add(
-  fmt 10.1.1
-  URL "https://github.com/fmtlib/fmt/archive/refs/tags/10.1.1.tar.gz"
-  URL_HASH SHA256=78b8c0a72b1c35e4443a7e308df52498252d1cefc2b08c9a97bc9ee6cfe61f8b
+  fmt 10.2.1
+  URL "https://github.com/fmtlib/fmt/archive/refs/tags/10.2.1.tar.gz"
+  URL_HASH SHA256=1250e4cc58bf06ee631567523f48848dc4596133e163f02615c97f78bab6c811
 )
