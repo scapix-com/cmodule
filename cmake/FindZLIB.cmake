e33fbb76b614158a8af2b53269485cfd0f10473a
Boris Rasin <boris@pointx.org>
2024-01-30 16:54:39 +0200

    optimize zlib config

--- cmake/FindZLIB.cmake
+++ cmake/FindZLIB.cmake
@@ -1,13 +1,13 @@
 include_guard(GLOBAL)
 
+option(ZLIB_BUILD_EXAMPLES "" OFF)
+option(SKIP_INSTALL_ALL "zlib" ON)
+
 cmodule_add(
   zlib 1.3.1
   URL      "https://github.com/madler/zlib/releases/download/v1.3.1/zlib-1.3.1.tar.xz"
   URL_HASH SHA256=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
 )
-
-target_include_directories(zlib INTERFACE ${CMODULE_zlib_SOURCE_DIR} ${CMODULE_zlib_BINARY_DIR})
-target_include_directories(zlibstatic INTERFACE ${CMODULE_zlib_SOURCE_DIR} ${CMODULE_zlib_BINARY_DIR})
 
 if(BUILD_SHARED_LIBS)
   add_library(ZLIB::ZLIB ALIAS zlib)
