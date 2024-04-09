82de982041ae85daa0a63fffcfaf1c6404e7060a
Boris Rasin <boris@pointx.org>
2024-04-10 01:33:38 +0300

    add catch2 version 3.5.3

---
+++ cmake/FindCatch2.cmake
@@ -0,0 +1,7 @@
+include_guard(GLOBAL)
+
+cmodule_add(
+  catch2 3.5.3
+  URL "https://github.com/catchorg/Catch2/archive/refs/tags/v3.5.3.tar.gz"
+  URL_HASH SHA256=8d723b0535c94860ef8cf6231580fa47d67a3416757ecb10639e40d748ab6c71
+)
