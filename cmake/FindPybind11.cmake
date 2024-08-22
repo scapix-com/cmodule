6fd1e044d01a15f4c3c840fb94f86230d1897d61
Boris Rasin <boris@pointx.org>
2024-08-22 22:16:03 +0300

    update pybind11 to version 2.13.5

--- cmake/FindPybind11.cmake
+++ cmake/FindPybind11.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 cmodule_add(
-  pybind11 2.13.4
-  URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.13.4.tar.gz"
-  URL_HASH SHA256=efc901aa0aab439a3fea6efeaf930b5a349fb06394bf845c64ce15a9cf8f0240
+  pybind11 2.13.5
+  URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.13.5.tar.gz"
+  URL_HASH SHA256=b1e209c42b3a9ed74da3e0b25a4f4cd478d89d5efbb48f04b277df427faf6252
 )
