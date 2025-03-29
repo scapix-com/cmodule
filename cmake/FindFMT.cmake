3187a61ea82e19a71c1f7fc47a0cf5d60b7a4826
Boris Rasin <boris@pointx.org>
2025-03-29 04:15:38 +0300

    update fmt to version 11.1.4

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 option(FMT_INSTALL "" OFF)
 
 cmodule_add(
-  fmt 11.1.1
-  URL "https://github.com/fmtlib/fmt/archive/refs/tags/11.1.1.tar.gz"
-  URL_HASH SHA256=482eed9efbc98388dbaee5cb5f368be5eca4893456bb358c18b7ff71f835ae43
+  fmt 11.1.4
+  URL "https://github.com/fmtlib/fmt/archive/refs/tags/11.1.4.tar.gz"
+  URL_HASH SHA256=ac366b7b4c2e9f0dde63a59b3feb5ee59b67974b14ee5dc9ea8ad78aa2c1ee1e
 )
