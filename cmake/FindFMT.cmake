d7da699bcc66ebf2ffe0baa498e816b668ea6e1e
Boris Rasin <boris@pointx.org>
2025-11-01 17:37:45 +0200

    update fmt to version 12.1.0

--- cmake/FindFMT.cmake
+++ cmake/FindFMT.cmake
@@ -3,7 +3,7 @@ include_guard(GLOBAL)
 option(FMT_INSTALL "" OFF)
 
 cmodule_add(
-  fmt 12.0.0
-  URL "https://github.com/fmtlib/fmt/archive/refs/tags/12.0.0.tar.gz"
-  URL_HASH SHA256=aa3e8fbb6a0066c03454434add1f1fc23299e85758ceec0d7d2d974431481e40
+  fmt 12.1.0
+  URL "https://github.com/fmtlib/fmt/archive/refs/tags/12.1.0.tar.gz"
+  URL_HASH SHA256=ea7de4299689e12b6dddd392f9896f08fb0777ac7168897a244a6d6085043fea
 )
