f3a02dfa22ee06d4b72a4e03f5b8071907a436ff
Boris Rasin <boris@pointx.org>
2026-04-28 00:32:26 +0300

    update zlib to version 1.3.2

--- cmake/FindZLIB.cmake
+++ cmake/FindZLIB.cmake
@@ -1,16 +1,10 @@
 include_guard(GLOBAL)
 
-option(ZLIB_BUILD_EXAMPLES "" OFF)
-option(SKIP_INSTALL_ALL "zlib" ON)
+option(ZLIB_BUILD_TESTING "" OFF)
+option(ZLIB_INSTALL "" OFF)
 
 cmodule_add(
-  zlib 1.3.1
-  URL      "https://github.com/madler/zlib/releases/download/v1.3.1/zlib-1.3.1.tar.xz"
-  URL_HASH SHA256=38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32
+  zlib 1.3.2
+  URL https://github.com/madler/zlib/releases/download/v1.3.2/zlib-1.3.2.tar.xz
+  URL_HASH SHA256=d7a0654783a4da529d1bb793b7ad9c3318020af77667bcae35f95d0e42a792f3
 )
-
-if(BUILD_SHARED_LIBS)
-  add_library(ZLIB::ZLIB ALIAS zlib)
-else()
-  add_library(ZLIB::ZLIB ALIAS zlibstatic)
-endif()
