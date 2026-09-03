7d50c229424b51dce0f0120f6da22ff0f19645a9
Boris Rasin <boris@pointx.org>
2026-09-04 02:18:07 +0300

    update scapix to version 2.5.0

--- cmake/FindScapixCore.cmake
+++ cmake/FindScapixCore.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 cmodule_add(
-  scapix_core 2.4.0
-  URL https://github.com/scapix-com/scapix-core/archive/refs/tags/v2.4.0.tar.gz
-  URL_HASH SHA256=dc25875aa99b2a50f7f768f0f238443ce41fca2866f67cb9857ddfaf027ee8b8
+  scapix_core 2.5.0
+  URL https://github.com/scapix-com/scapix-core/archive/refs/tags/v2.5.0.tar.gz
+  URL_HASH SHA256=35d21d84bece5b6058d4a232069235326d589821d8d3513cd4808c54c7d2775a
 )
--- cmake/FindScapixJNI.cmake
+++ cmake/FindScapixJNI.cmake
@@ -1,7 +1,7 @@
 include_guard(GLOBAL)
 
 cmodule_add(
-  scapix_jni 2.4.0
-  URL https://github.com/scapix-com/scapix-jni/archive/refs/tags/v2.4.0.tar.gz
-  URL_HASH SHA256=b582a98b81c72c7a598410b4669b90e62ed37c6b0d5bbc64c236afbc7faed980
+  scapix_jni 2.5.0
+  URL https://github.com/scapix-com/scapix-jni/archive/refs/tags/v2.5.0.tar.gz
+  URL_HASH SHA256=cb7cd5bc92da99e8c153a7958aed29a8a3be33c0375cf708c663f20e94e5a42a
 )
